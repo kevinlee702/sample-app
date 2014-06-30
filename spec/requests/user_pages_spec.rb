3c8a5ed690af885f77efcf5ac096cc3788a89917
Kevin Lee <kevinlee702@gmail.com>
2014-06-30 15:08:25 -0400

    Finish user edit, update, index, and destroy actions.  Functional has few fail tests.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,58 +1,132 @@
-require 'spec_helper'
+  require 'spec_helper'
 
-describe "User pages" do
+    describe "User pages" do
 
-  subject { page }
+      subject { page }
 
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
+      describe "index" do
+        let(:user) { FactoryGirl.create(:user) }
+        before do
+          sign_in user
+          visit users_path
+        end
 
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
-  end
+        it { should have_title('All users') }
+        it { should have_content('All users') }
 
-  describe "signup page" do
-    before { visit signup_path }
+        describe "pagination" do
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
-  end
+          before(:all) { 30.times { FactoryGirl.create(:user) } }
+          after(:all)  { User.delete_all }
 
-  describe "signup" do
+          it { should have_selector('div.pagination') }
 
-    before { visit signup_path }
+          it "should list each user" do
+            User.paginate(page: 1).each do |user|
+              expect(page).to have_selector('li', text: user.name)
+            end
+          end
+        end
 
-    let(:submit) { "Create my account" }
+        describe "delete links" do
 
-    describe "with invalid information" do
-      it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
-      end
-    end
+          it { should_not have_link('delete') }
 
-    describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+          describe "as an admin user" do
+            let(:admin) { FactoryGirl.create(:admin) }
+            before do
+              sign_in admin
+              visit users_path
+            end
 
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+            it { should have_link('delete', href: user_path(User.first)) }
+            it "should be able to delete another user" do
+              expect do
+                click_link('delete', match: :first)
+              end.to change(User, :count).by(-1)
+            end
+            it { should_not have_link('delete', href: user_path(admin)) }
+          end
+        end
       end
     
+  
+      
+      describe "signup page" do
+        before { visit signup_path }
 
-      describe "after saving the user" do
-          before { click_button submit }
-          let(:user) { User.find_by_email('user@example.com') }
+        it { should have_selector('h1',    text: 'Sign up') }
+        it { should have_selector('title', text: 'Sign up') }
+      end
 
-          it { should have_link('Sign out') }
-          it { should have_title(user.name) }
-          it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      describe "profile page" do
+        let(:user) { FactoryGirl.create(:user) }
+        before { visit user_path(user) }
+
+        it { should have_selector('h1',    text: user.name) }
+        it { should have_selector('title', text: user.name) }
+      end
+
+      describe "signup" do
+
+        before { visit signup_path }
+
+        let(:submit) { "Create my account" }
+
+        describe "with invalid information" do
+          it "should not create a user" do
+            expect { click_button submit }.not_to change(User, :count)
+          end
+
+          describe "after submission" do
+            before { click_button submit }
+
+            it { should have_selector('title', text: 'Sign up') }
+            it { should have_content('error') }
+            it { should_not have_content('Password digest') }
+          end
+        end
+
+        describe "with valid information" do
+
+          before do
+            fill_in "Name",         with: "Example User"
+            fill_in "Email",        with: "user@example.com"
+            fill_in "Password",     with: "foobar"
+            fill_in "Confirmation", with: "foobar"
+          end
+
+          it "should create a user" do
+            expect { click_button submit }.to change(User, :count).by(1)
+          end
+
+          describe "after saving the user" do
+            before { click_button submit }
+
+            let(:user) { User.find_by_email('user@example.com') }
+
+            it { should have_selector('title', text: user.name) }
+            it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+            it { should have_link('Sign out') }
+          end
+        end
+      end
+
+      describe "edit" do
+        let(:user) { FactoryGirl.create(:user) }
+        before { visit edit_user_path(user) }
+
+        describe "page" do
+          it { should have_content("Update your profile") }
+          it { should have_title("Edit user") }
+          it { should have_link('change', href: 'http://gravatar.com/emails') }
+        end
+
+        describe "with invalid information" do
+          before { click_button "Save changes" }
+
+          it { should have_content('error') }
+        end
+
       end
     end
- end 
-
-end
